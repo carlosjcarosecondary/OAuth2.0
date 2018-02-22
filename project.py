6cec2c6b3a5b5ef5ae4a88cec3840c8cb5f9abf8
Carlos Caro <carlosjesuscaro@icloud.com>
2018-02-21 23:31:55 -0500

    Adding User tools

--- project.py
+++ project.py
@@ -21,7 +21,7 @@ CLIENT_ID =json.loads(open('client_secrets.json','r').read())['web']['client_id'
 APPLICATION_NAME = 'Restaurant Menu Application'
 
 #Connect to Database and create database session
-engine = create_engine('sqlite:///restaurantmenu.db')
+engine = create_engine('sqlite:///restaurantmenuwithusers.db')
 Base.metadata.bind = engine
 
 DBSession = sessionmaker(bind=engine)
@@ -103,6 +103,26 @@ def gconnect():
     flash("you are now logged in as %s" % login_session['username'])
     print "done!"
     return output
+
+# User tools
+def createUser(login_session):
+    newUser = User(name = login_session['username'], email = login_session['email'],
+                  picture = login_session['picture'])
+    session.add(newUser)
+    session.commit()
+    user = session.query(User).filter_by(email = login_session['email']).one()
+    return user.id
+
+def getUserInfo(user_id):
+    user = session.query(User).filter_by(id = user_id).one()
+    return user
+
+def getUserID(email):
+    try:
+      user = session.query(User).filter_by(id = user_id).one()
+      return user.id
+    except:
+      return None
 
 # Disconnecting the session from Google Plus
 @app.route('/gdisconnect')
